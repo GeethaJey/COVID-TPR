127aeda7dbb6cd117db090bbaa95c14231dca46e
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-07-13 10:28:40 -0400

    Meeting with Geetha

--- Regression.R
+++ Regression.R
@@ -1,28 +1,48 @@
 library(ggplot2)
 library(dplyr)
-library(olsrr)
 
 #Canada TPR and policy measure dataset 
-df <- read.csv("~/Desktop/df.csv")
+canada <- read.csv("df.csv")
+
 # All policy and demographic measures from DELVE dataset
-X = read.csv(url("https://raw.githubusercontent.com/rs-delve/covid19_datasets/master/dataset/combined_dataset_latest.csv"))
 # Change all necessary variables to categorical 
- df <- df %>% mutate_if (is.integer, as.factor)
- X <- X %>% mutate_at (vars(matches("npi")), as.factor)
+canada <- (canada 
+	%>% mutate_if (is.integer, as.factor)
+	%>% mutate(date = as.Date(date, format="%d-%m-%Y"))
+)
+
+## delve = read.csv(url("https://raw.githubusercontent.com/rs-delve/covid19_datasets/master/dataset/combined_dataset_latest.csv"))
+## delve <- delve %>% mutate_at (vars(matches("npi")), as.factor)
+## summary(delve)
 
 # Summary statistics
-str(df)
-str(X)
+summary(canada)
 
 # Data visualization 
+## Time plots
+print(
+	ggplot(canada)
+	+ aes(x = date, y= tpr)
+	+ geom_point()
+	+ ylab("Positivity (percent)")
+)
+
+## Time plots
+print(
+	ggplot(canada)
+	+ aes(x = date, y= Test.rate)
+	+ geom_point()
+	+ ylab("Test rate (UNKNOWN units)")
+)
+
 # Scatter plot of test rate and test positivity rate
-ggplot(df, aes(x = Test.rate, y= tpr, group=prname)) + 
+ggplot(canada, aes(x = Test.rate, y= tpr, group=prname)) + 
   geom_point(aes(color=prname)) + xlab("Test Rate") + 
   ylab("Test Positivity Rate") +
   ggtitle("Effect of test rate on test positivity rate")
 
 # Scatter plot of government stringency index and test positivity rate
-ggplot(df, aes(x = government.stringency.index, y= tpr, group=prname)) + 
+ggplot(canada, aes(x = government.stringency.index, y= tpr, group=prname)) + 
   geom_point(aes(color=prname)) + xlab("Government Stringency Index") + 
   ylab("Test Positivity Rate") +
   ggtitle("Effect of government stringency index on test positivity rate")
@@ -35,7 +55,7 @@ model <- lm(tpr ~ poly(government.stringency.index, degree=2, raw=T) +
               testing.policy + international.travel.controls +
               restrictions.on.public.gatherings + school.closures +
               income.relief +
-              debt.or.contract.relief, data=df)
+              debt.or.contract.relief, data=canada)
 
 # Choose the best model subset based on AIC (Akaike’s Information Criteria)
 #ols_step_best_subset(model)
---
+++ TODO.md
@@ -0,0 +1,18 @@
+Geetha
+======
+* Read a short Intro to markdown
+* Update README and try to get consensus on what we are doing
+* Document where the Canada data are coming from
+	* or else, use the Delve data first?
+
+* Don't do statistics until we have a statistics plan
+
+* For now: make a plan for what data to download, how to organize it and how to visualize it
+	* See if you can figure out how to control the dates on whatever data you want to use
+	* Let JD work with you on caching data
+
+Together
+========
+
+* Work on download pipeline and data caching
+
