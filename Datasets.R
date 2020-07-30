be116043a2250c30f157b7bee22dab0aa9222acd
Geetha Jeyapragasan <jeyaprag@mcmaster.ca>
2020-07-30 13:45:37 -0400

    Updates

--- Datasets.R
+++ Datasets.R
@@ -10,6 +10,7 @@ owid <- read.csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 owid <- (owid %>% 
         mutate(location = as.factor(location), continent = as.factor(continent), iso_code = as.factor(iso_code), date = (as.Date(date, format= "%Y-%m-%d")),tests_units = as.factor(tests_units)) %>% 
         mutate(cumulative.tpr = (total_cases/total_tests)*100, daily.tpr = (new_cases/new_tests)*100) %>% 
+        select (!(total_cases:stringency_index)) %>%         
         filter(!is.na(daily.tpr)) %>% 
         relocate(daily.tpr,cumulative.tpr) %>% 
         rename(country = location))
@@ -28,8 +29,43 @@ summary(delve)
 #Country Effective Reproductive Number Estimates from University of Oxford, Australian National University, and Harvard from http://epidemicforecasting.org/
 Rt <- read.csv("https://storage.googleapis.com/static-covid/static/v4/main/r_estimates.csv")
 Rt <- (Rt %>%
-         mutate(Date = as.Date(Date, format= "%Y-%m-%d"), X=NULL) %>%
+         mutate(Date = as.Date(Date, format= "%Y-%m-%d"), X = NULL) %>%
          filter(!grepl ("^US-", Code)) %>% 
-         mutate(country = countrycode(Code, "iso2c","country.name"),country =as.factor(country)) %>% #converting country codes to country names. US states not converted 
+         mutate(country = countrycode(Code, "iso2c","country.name"), country =as.factor(country)) %>% #converting country codes to country names. US states not converted 
          rename(date=Date))
 summary(Rt)
+
+# Putting together global Dataset
+global <- inner_join(owid, delve)
+global <- inner_join(global,Rt) 
+                
+summary(global)
+
+# US DATA 
+############
+
+#Covid-Tracking project
+US_data <- read.csv("https://s3-us-west-1.amazonaws.com/starschema.covid/CT_US_COVID_TESTS.csv")
+
+
+#Canada Data
+#Government of Canada (GoC) COVID-19 Dataset 
+goc = read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19.csv")
+goc_metadata = read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-data-dictionary.csv")
+
+goc <- (goc %>%
+                mutate (pruid = NULL,prnameFR = NULL) %>% 
+                mutate(date = as.Date(date, format= "%d-%m-%Y")) %>%
+                mutate (prname = as.factor(prname)) %>%
+                relocate(country) %>%
+                mutate (cumulative.tpr = (numconf/numtested)*100))
+
+summary(goc)
+# Create Subset to for only National Canada Data
+national <- inner_join(goc, delve)
+national <- inner_join(national, Rt)
+national <- (national %>% 
+                     filter(country == "Canada" & !is.na(tpr)) %>%
+                     mutate(npi_masks = as.factor(npi_masks)) %>% 
+                     mutate(npi_testing_policy = as.factor(npi_testing_policy)) %>% 
+                     mutate(Code = NULL, X = NULL, Date = NULL))
--- Regression.R
+++ Regression.R
@@ -2,55 +2,11 @@ library(ggplot2)
 library(dplyr)
 library(countrycode)
 
-#Government of Canada (GoC) COVID-19 Dataset 
-goc = read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19.csv")
-goc_metadata = read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-data-dictionary.csv")
-
-goc <- (goc %>%
-  mutate (pruid = NULL,prnameFR = NULL) %>% 
-  mutate(date = as.Date(date, format= "%d-%m-%Y")) %>%
-  mutate (country = as.factor(prname)) %>%
-  relocate(country) %>%
-  mutate (tpr = (numconf/numtested)*100) %>%
-  mutate (prname = NULL) %>% 
-  filter(country == "Canada"))
-
-summary(goc)
-
-#DELVE Global Interventions Dataset *transmute function always seems to give me an error 
-##Codebook with variable descriptions https://github.com/rs-delve/covid19_datasets/blob/master/docs/codebook.md
-
-delve = read.csv(url("https://raw.githubusercontent.com/rs-delve/covid19_datasets/master/dataset/combined_dataset_latest.csv"))
- delve <- (delve %>%
-   mutate(date = as.Date(DATE, format= "%Y-%m-%d")) %>%
-   mutate(country = as.factor(country_name)) %>%
-   mutate(ISO=NULL) %>%
-   relocate (date, country) %>%
-   mutate (DATE = NULL, country_name = NULL, iso_3166_2_code = NULL, census_fips_code = NULL) ) 
-
- summary(delve)
-
-
-#Country Effective Reproductive Number Estimates from University of Oxford, Australian National University, and Harvard from http://epidemicforecasting.org/
-
-
-
-# Create Subset to for only National Canada Data
-national <- inner_join(goc, delve)
-national <- inner_join(national, Rt)
-national <- (national %>% 
-            filter(country == "Canada" & !is.na(tpr)) %>%
-            mutate(npi_masks = as.factor(npi_masks)) %>% 
-            mutate(npi_testing_policy = as.factor(npi_testing_policy)) %>% 
-            mutate(Code = NULL, X = NULL, Date = NULL))
-
-summary(national)
-
 # Data visualization 
-## Time plots for TPR
+## Time plots for Global Cumulative TPR
 print(
-	ggplot(national)
-	+ aes(date, tpr)
+	ggplot(global)
+	+ aes(date, cumulative.tpr, colour = continent)
 	+ geom_point()
 	+ ylab("Cumulative Positivity Rate (%)")
 	+ xlab("Date")
@@ -58,24 +14,20 @@ print(
 
 ## Time plots for Testing Rate
 print(
-	ggplot(national) +
-  aes(date, ratetested) +
+	ggplot(global) +
+  aes(date, tests_total_per_thousand, colour = continent) +
 	geom_point() +
-	ylab("Cumulative Test rate (per 1 million people)")
+	ylab("Cumulative Test rate (per 1 thousand people)")
 )
 
-
 #Regressions 
-# Model 1: Cumulative TPR ~ Cumulative Testing Rate + testing policy 
-
-model1 <- lm(tpr ~  ratetested + npi_testing_policy, data = national)
+# Model 1: Cumulative TPR ~ testing rate + effective reproduction number + government stringency index + mask policy
+model1 <- lm(cumulative.tpr ~  tests_total_per_thousand + MeanR + npi_stringency_index, data = global)
 summary(model1)
 plot(model1) 
  
 
-# Model 2: Cumulative TPR ~ testing rate + testing policy + effective reproduction number + government stringency index + mask policy
-model2 <- lm(tpr ~  ratetested + npi_testing_policy+ MeanR + npi_stringency_index, data = national)
+# Model 2: 
+model2 <- lm(cumulative.tpr ~ npi_testing_policy + tests_total_per_thousand, data = global)
 summary(model2)
-plot(model2) 
- 
-
+plot(model2)
